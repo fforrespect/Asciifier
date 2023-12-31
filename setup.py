2695bfe5effaf379fc040c79f8c5cdcd6fe7c991
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2023-12-31 18:23:19 +0400

    Ready for distribution

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name="asciifier",
     packages=find_packages(include=["asciifier"]),
-    version="0.1.0",
+    version="1.0.0",
     description="Converts an Image file into ASCII Art",
     author="fforrespect",
-    install_requires=["Pillow==10.1.0", "numpy==1.26.2", "setuptools==69.0.3"],
+    install_requires=["Pillow==10.1.0", "numpy==1.26.2", "requests==2.31.0"],
     setup_requires=["pytest-runner"],
     tests_require=["pytest==4.4.1"],
     test_suite="tests",
