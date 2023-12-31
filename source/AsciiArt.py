e0db2740c800c96f5e2995d22252a792e72b14a0
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2023-12-31 16:52:43 +0400

    Added docstrings and typehints

--- source/AsciiArt.py
+++ source/AsciiArt.py
@@ -1,27 +1,37 @@
 from PIL import Image
-import numpy as np
+from numpy import array, ndarray
 
 greys = (' ', '.', '-', '"', 'r', '/', '>', ')', '[', 'I', 'Y', 'Z', 'h', '#', '8', '@')
 
 
-def image_to_string(image_file_path, colours=greys):
+def image_to_string(file_path, in_place=True, colours=greys) -> str | None:
     """
     Convert an image to ASCII art.
-
-    Parameters:
-    image_file_path (str): Path to the image file.
-    in_place (bool): If True, prints the ASCII art. If False, returns the ASCII art as a string.
-    colours (tuple): A tuple of characters representing grayscale values.
-
-    Returns:
-    str: ASCII art representation of the image (if in_place is False).
+    
+    :param file_path: Path to the image file.
+    :type file_path: str
+    :param in_place: If True, prints the ASCII art. If False, returns the ASCII art as a string.
+    :type in_place: bool
+    :param colours: A tuple of characters representing greyscale values.
+    :type colours: tuple
+    
+    :returns ASCII art representation of the image (if in_place is False)
+    :rtype str
     """
-    image = Image.open(image_file_path)
-    image_array = np.array(image)
-    greyscale = len(image_array.shape) == 2
 
-    colour_scale = len(colours) - 1
-    normalised_image = (image_array.mean(axis=2) / 255 * colour_scale if not greyscale
-                        else image_array / 255 * colour_scale).astype(int)
+    image: Image.Image = Image.open(file_path)
+    # The following line may throw a warning, there's no reason it should - it will always work fine
+    # noinspection PyTypeChecker
+    image_array: ndarray = array(image)
+    greyscale: bool = len(image_array.shape) == 2
 
-    return "\n".join("".join(colours[val] * 2 for val in row) for row in normalised_image)
+    colour_scale: int = len(colours) - 1
+    normalised_image: ndarray = (image_array.mean(axis=2) / 255 * colour_scale if not greyscale
+                                 else image_array / 255 * colour_scale).astype(int)
+
+    if in_place:
+        for row in normalised_image:
+            print("".join(colours[val] * 2 for val in row))
+        return None
+    else:
+        return "\n".join("".join(colours[val] * 2 for val in row) for row in normalised_image)
