4b97b84b9b5729abc1d9ea8f911d6925bf520266
Jaiveer Chadda <jaiveer.chadda@gmail.com>
2024-01-20 15:52:23 +0000

    New Release 1.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name="asciifier",
     packages=find_packages(include=["asciifier"]),
-    version="1.0.1",
+    version="1.2.0",
     description="Converts an Image file into ASCII Art",
     author="jaiveer chadda",
     author_email='jaiveer.chadda@gmail.com',
